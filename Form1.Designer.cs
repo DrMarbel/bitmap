a59673de10b1b3878f7936d211bf7bdf097e4ba1
DrMarbel <martinbelt753@gmail.com>
2021-05-10 17:37:51 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace bitmapGeneraterGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1018, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1018, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1015, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "null";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1015, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "null";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bitmap Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bitmapGeneraterGUI
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> tiles = new List<PictureBox>();
+        int mapWidth = 16;
+        int mapHeight = 16;
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < mapHeight * mapWidth; i++)
+            {
+                tiles.Add(new PictureBox());
+                tiles[tiles.Count - 1].Size = new Size(16, 16);
+                tiles[tiles.Count - 1].Location = new Point(200,-50);
+                tiles[tiles.Count - 1].Location = new Point((i / mapWidth) * 16, (i % mapWidth) * 16);
+                this.Controls.Add(tiles[tiles.Count - 1]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("*Map Generated*");
+            int[,] arr = StandinWorldGenerator();
+            /*string output = "";
+            for (int i = 0; i < arr.Length && i < 1; i++)
+            {
+                for (int a = 0; a < arr.Length && a < 1; a++)
+                {
+                    output += Convert.ToString(arr[0,0]);
+                }
+            }
+            label1.Text = output;
+            */
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    switch (arr[x, y])
+                    {
+                        case 0:
+                            tiles[x * mapWidth + y].ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\sand.png";
+                            break;
+                        case 1:
+                            tiles[x * mapWidth + y].ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\water.png";
+                            break;
+                        case 2:
+                            tiles[x * mapWidth + y].ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\grass.png";
+                            break;
+                        case 3:
+                            tiles[x * mapWidth + y].ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\stone.png";
+                            break;
+                        case 4:
+                            tiles[x * mapWidth + y].ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\grass2.png";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+
+            // label1.Text = Convert.ToString(arr[0,0]);
+            // label1.Text = output;
+            
+            // WORKS!
+            /*
+            if (arr[0,0] == 0)
+            {
+                // label1.Text = "TEST";
+                label2.Text = "*Sand*";
+
+                pictureBox1.ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\sand.png";
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            } else if (arr[0, 0] == 1) {
+                label2.Text = "*Water*";
+
+                pictureBox1.ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\water.png";
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            else if (arr[0, 0] == 2)
+            {
+                label2.Text = "*Grass*";
+
+                pictureBox1.ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\grass.png";
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            else if (arr[0, 0] == 3)
+            {
+                label2.Text = "*Stone*";
+
+                pictureBox1.ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\stone.png";
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            else if (arr[0, 0] == 4)
+            {
+                label2.Text = "*Grass 2*";
+
+                pictureBox1.ImageLocation = "C:\\Users\\Martin\\Pictures\\Saved Pictures\\Tiles\\grass2.png";
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            */
+        }
+
+        int[,] StandinWorldGenerator()
+        {
+            int col = this.mapWidth;
+            int rowThing = this.mapHeight;
+            Random random = new Random();
+            int[,] Output = new int[col, rowThing];
+            for (int collumn = 0; collumn < col; collumn++)
+            {
+                for (int row = 0; row < rowThing; row++)
+                {
+                    Output[row, collumn] = random.Next(0, 5);
+                }
+            }
+            return Output;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult confrim = MessageBox.Show("Do you really want to leave?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confrim == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
